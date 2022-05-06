7894e8d698015bc3b3483ab40432520371930b67
metlash <51310264+metlash@users.noreply.github.com>
2022-05-06 12:41:57 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,36 @@
+﻿using FootBall.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.SqlClient;
+using System.IO;
+using System.Data.Entity;
+
+
+namespace FootBall.Controllers
+{
+    public class HomeController : Controller
+    {
+        PlayDbContext context = new PlayDbContext();
+        public ActionResult Index()
+        {
+            return View(context.Teams);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        public ActionResult CreateAccount(Team a)
+        {
+            context.Teams.Add(a);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ PlayDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using FootBall.Models;
+
+namespace FootBall.Models
+{
+    public class PlayDbContext : DbContext
+    {
+        public PlayDbContext()
+            : base("name=conn")
+        {
+
+        }
+        public DbSet<Team> Teams { get; set; }
+
+    }
+}
