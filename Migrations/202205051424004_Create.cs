0f1d86ccc4d846a129b5db7b3e1d6fee63f4af10
metlash <51310264+metlash@users.noreply.github.com>
2022-05-09 19:28:31 +0530

    Add files via upload

---
+++ Migrations/202205051424004_Create.cs
@@ -0,0 +1,30 @@
+﻿namespace FootBall.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Create : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Teams",
+                c => new
+                    {
+                        MatchID = c.Int(nullable: false),
+                        TeamName1 = c.String(nullable: false),
+                        TeamName2 = c.String(nullable: false),
+                        Status = c.String(nullable: false),
+                        WinningTeam = c.String(),
+                        Points = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.MatchID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Teams");
+        }
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FootBall.Models
+{
+    public class Team
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Required]
+        public int MatchID { get; set; }
+
+        [Required]
+        public string TeamName1 { get; set; }
+
+        [Required]
+
+        public string TeamName2 { get; set; }
+
+        [Required]
+
+        public string Status { get; set; }
+
+
+        public string WinningTeam { get; set; }
+
+        [Required]
+
+        public int Points { get; set; }
+
+    }
+}
